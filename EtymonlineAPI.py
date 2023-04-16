91bdf027bd611250d91ff62d2ffad02768a5635e
Matthew Pickering <wyomatthew@hotmail.com>
2023-04-16 19:10:17 -0400

    Modifying origin lookup to return word description was associated with

--- EtymonlineAPI.py
+++ EtymonlineAPI.py
@@ -8,6 +8,7 @@ import bs4
 import requests as r
 from bs4 import BeautifulSoup
 from nltk.tokenize import word_tokenize
+from nltk.metrics import edit_distance
 
 BASE_URL = "https://www.etymonline.com"
 hot_list = ["french", "latin", "german", "english"]
@@ -25,8 +26,7 @@ def make_request(url: str, params: dict = None, headers: dict = None) -> r.Respo
     ConnectionError
         On any non-ok response code"""
     res = r.get(url, params=params, headers=headers)
-    with open("out.html", "w") as fp:
-        fp.write(res.text)
+
     if not res.ok:
         raise ConnectionError(f"Failed to reach {url}, received {res.status_code} : {res.reason}\n{res.text}")
 
@@ -46,12 +46,16 @@ def get_etymonline_description(word: str) -> str:
     etymology_text = soup.body.find("section").p.get_text()
     return etymology_text
 
-def get_etymonline_search(word: str) -> list[str]:
+def get_etymonline_search(word: str) -> list[tuple[str, str]]:
     url = f"{BASE_URL}/search"
     res = make_request(url, params={"q": word})
 
     soup = BeautifulSoup(res.text, features="html.parser")
-    return [tag.get_text() for tag in soup.find_all("section")]
+    target_class = "word--C9UPa"
+    tags: list[bs4.Tag] = soup.find_all("div", {"class": target_class})
+    outputs = [(tag.find("div").find("a").get_text().split("\xa0")[0], tag.find("div").find("div").find("section").find("p").get_text()) for tag in tags]
+
+    return sorted(outputs, key=lambda tup: edit_distance(word, tup[0], substitution_cost=2)) 
 
 def find_language(words: list[str]) -> Optional[str]:
     for word in words:
@@ -61,15 +65,22 @@ def find_language(words: list[str]) -> Optional[str]:
     
     return None
 
-def lookup_origin(word: str) -> Optional[str]:
-    etymology_texts = get_etymonline_search(word)
-    for text in etymology_texts:
-        text = text.lower()
+def lookup_origin(word: str) -> Optional[tuple[str, str]]:
+    etymons: list[tuple[str, str]] = list()
+    try:
+        etymonline_desc = get_etymonline_description(word)
+        etymons.append((word, etymonline_desc))
+    except ConnectionError:
+        for tup in get_etymonline_search(word):
+            etymons.append(tup)
+
+    for word, description in etymons:
+        text = description.lower()
         tokens = word_tokenize(text)
         words = list(filter(lambda word: match(r"\w+", word) is not None, tokens))
 
         language = find_language(words)
         if language is not None:
-            return language
-    
+            return (word, language)
+
     return None
